1c0dcfd541d0a34bc2b365167e4f0b0565c18301
Matthew McGrath <matthew.mcgrath12@gmail.com>
2025-04-06 10:28:53 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # try-smolagents
 Try out smolagents
+
+## Telemetry
+
+`python -m phoenix.server.main serve`
+
+## Run
+
+`uv run hello.py`
--- hello.py
+++ hello.py
@@ -1,16 +1,43 @@
 from dotenv import load_dotenv
 import os
 
-from smolagents import CodeAgent, DuckDuckGoSearchTool, HfApiModel
+from phoenix.otel import register
+from openinference.instrumentation.smolagents import SmolagentsInstrumentor
+
+from smolagents import (
+    CodeAgent,
+    ToolCallingAgent,
+    DuckDuckGoSearchTool,
+    VisitWebpageTool,
+    HfApiModel,
+)
+
+register()
+SmolagentsInstrumentor().instrument()
+
 
 load_dotenv()
 
-model = HfApiModel(token=os.getenv("HF_TOKEN"))
-agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=model)
+model = HfApiModel(token=os.getenv("HF_TOKEN"), max_tokens=5000)
+search_agent = ToolCallingAgent(
+    tools=[DuckDuckGoSearchTool(), VisitWebpageTool()],
+    model=model,
+    name="search_agent",
+    description="This is an agent that can do web search.",
+)
+
+manager_agent = CodeAgent(
+    tools=[],
+    model=model,
+    managed_agents=[search_agent],
+)
 
 
 def main():
-    res = agent.run("how many times would you have to dribble a basketball before it popped?")
+    res = manager_agent.run(
+        "did duke or houston win the last game?"
+    )
+
     print(res)
 
 
